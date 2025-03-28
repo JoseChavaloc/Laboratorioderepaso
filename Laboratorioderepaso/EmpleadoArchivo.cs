cbe612719b0896be66860daf83b7a9d7c1e053dd
chava <josechavaloc@umes.edu.gt>
2025-03-27 19:32:38 -0600

    cambio de archivo

--- Laboratorioderepaso/EmpleadoArchivo.cs
+++ Laboratorioderepaso/EmpleadoArchivo.cs
@@ -18,15 +18,25 @@ namespace Laboratorioderepaso
         {
 
 
-            List<Empleado> lista = new List<Empleado>();
+            // Verificar si el archivo no existe
+            if (!File.Exists(archivo))
+            {
+                // Crear una lista vacía
+                List<Empleado> listaVacia = new List<Empleado>();
+
+                // Serializar y guardar esa lista vacía como JSON
+                string jsonVacio = JsonConvert.SerializeObject(listaVacia, Formatting.Indented);
+                File.WriteAllText(archivo, jsonVacio);
+            }
+
+            // Leer el contenido del archivo
             StreamReader jsonStream = File.OpenText(archivo);
-
             string json = jsonStream.ReadToEnd();
-
             jsonStream.Close();
 
+            // Deserializar el contenido a una lista de empleados
+            List<Empleado> lista = JsonConvert.DeserializeObject<List<Empleado>>(json);
 
-            lista = JsonConvert.DeserializeObject<List<Empleado>>(json);
             return lista;
         }
 
