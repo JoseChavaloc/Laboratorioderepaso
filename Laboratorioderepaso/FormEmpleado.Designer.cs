1a17c56c77518479a1ada8b36dd41fd8fa2b16d3
chava <josechavaloc@umes.edu.gt>
2025-03-27 18:02:57 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorioderepaso/FormEmpleado.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Laboratorioderepaso
+{
+    partial class FormEmpleado
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownEmpleado = new System.Windows.Forms.NumericUpDown();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.maskedTxtSueldo = new System.Windows.Forms.MaskedTextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAsistencia = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEmpleado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No. de Empleado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre de Empleado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sueldo por Hora";
+            // 
+            // numericUpDownEmpleado
+            // 
+            this.numericUpDownEmpleado.Location = new System.Drawing.Point(254, 95);
+            this.numericUpDownEmpleado.Name = "numericUpDownEmpleado";
+            this.numericUpDownEmpleado.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownEmpleado.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(254, 150);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 22);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // maskedTxtSueldo
+            // 
+            this.maskedTxtSueldo.Location = new System.Drawing.Point(254, 207);
+            this.maskedTxtSueldo.Mask = "999.99";
+            this.maskedTxtSueldo.Name = "maskedTxtSueldo";
+            this.maskedTxtSueldo.Size = new System.Drawing.Size(100, 22);
+            this.maskedTxtSueldo.TabIndex = 6;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(121, 397);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(151, 59);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar Empleado";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Location = new System.Drawing.Point(486, 448);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(134, 48);
+            this.btnReporte.TabIndex = 8;
+            this.btnReporte.Text = "Ir a Reporte";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(496, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(393, 233);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // btnAsistencia
+            // 
+            this.btnAsistencia.Location = new System.Drawing.Point(486, 328);
+            this.btnAsistencia.Name = "btnAsistencia";
+            this.btnAsistencia.Size = new System.Drawing.Size(151, 51);
+            this.btnAsistencia.TabIndex = 10;
+            this.btnAsistencia.Text = "Ingresar Asistencia";
+            this.btnAsistencia.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(741, 449);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(132, 47);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // FormEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 555);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnAsistencia);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnReporte);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.maskedTxtSueldo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.numericUpDownEmpleado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEmpleado";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormEmpleado_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEmpleado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownEmpleado;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.MaskedTextBox maskedTxtSueldo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnReporte;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAsistencia;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
